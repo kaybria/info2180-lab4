169275b452f1b672fe09f81dcc674dd21539608b
Kali Soutar <kalisoutar@gmail.com>
2021-10-30 13:29:47 -0500

    updated exercise 2

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 window. onload= function(){
     let theRequest= new XMLHttpRequest () ;
-    let file = "http://localhost/info2180-lab4/superheroes.php"
+    let superherofile = "http://localhost/info2180-lab4/superheroes.php"
     const load= document.querySelector("#search")
 
     load.addEventListener("click", function (e){
-        theRequest.open ("GET", file) ;
+        theRequest.open ("GET", superherofile) ;
         theRequest.send();   
         e.preventDefault();
         theRequest. onreadystatechange = function(){
